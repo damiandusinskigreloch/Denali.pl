280fc8fe554cbaa3782b53795c3132f688eafe4f
Damian Dusiński-Greloch <damiandusinskigreloch@gmail.com>
2019-07-25 07:12:35 +0200

    Init

---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.querySelector(".menu-btn").addEventListener("click", function() {
+  document.querySelector(".menu-top").classList.toggle("active");
+  document.querySelector("nav.menu").classList.toggle("active");
+});
